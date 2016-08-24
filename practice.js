0efda1c03913393c802d3f82a47fefa9dc4de1f8
Austin Walker <Austin Walker>
2016-08-24 16:14:57 -0600

    W2D2 Reuploaded for safety

--- practice.js
+++ practice.js
@@ -2,15 +2,78 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
-
+      /* bind methond for execution context */
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
+      /* function myFunc(){
+            console.log(this.name);
+         }
+         Explicit Binding
+          function myFunc(param1, param2){
+           console.log(this.name, param1, param2);
+          }
+          myFunc();
 
+          var data = ['three, 'four']
+          myFunc.call({name:"DM13"}, "one", "two")
+          myFunc.apply({name:"DM13"}, data)
+
+          var dm15Func = myFunc.bind({name:"DM15"});
+          var dm16Func = myFunc.bind({name:"DM15"});
+
+          dm15Func(1, 3);
+          dm15Func(2, 3);
+          dm16Func(1, 7); */
+      /*  Implicit Binding
+           function calcRaceTimes() {
+             return Math.average(this.laps, this.name);
+           }
+           var person = {
+               name: "Davey",
+               laps:[12, 15, 24, 17, 90, 67],
+               personFunc: myFunc,
+               calculateRactTime: calcRaceTimes
+
+           }
+           var person2 = {
+               name: "Smavey",
+               personFunc: myFunc
+           }
+
+           person.personFunc(3, 6);
+           person2.personFunc(5, 5);
+
+           person.calculateRactTime(); */
+      /* Default/Window
+           function func2() {
+             console.log(this.title);
+             this.title = "mwhahahaha";
+           }
+
+           func2(); */
+      /* New Binding - new
+           function Animal(size, legs, sound) {
+             //this = {}
+             //this.size = size;
+             //this.legs = legs;
+             //this.sound = sound;
+             this.makeSound = function() {
+                 console.log(this.sound);
+             }
+             this.run = function () {
+                 console.log('I run on ' + this.legs + ' legs.')
+             }
+             //return this
+           }
+
+           var giraffe = new Animal('large, 4, 'large goat bleet');
+           var zebra = new Animal('medium', 4, 'whoop whoop');
+           var monkey = new Animal('small', 2, 'ooh ohh');
   // 3) What is the difference between call and apply?
 
       //Answer
-
+      /*
   // 4) What does .bind do?
 
       //Answer
@@ -23,8 +86,14 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
-
+    //Code Here*/
+var user = {
+  username:"BGBRWR",
+  email:"bgbrwr@gmail.com",
+  getUsername: function() {
+    return this.username;
+  }
+ }
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
 
@@ -34,6 +103,16 @@
 // Write the function definitions which will make the following function invocations function properly.
 
   //Function Invocations Here
+var Car = function(make, model, year){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    this.move += 10;
+    return this.move;
+  }
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -69,7 +148,7 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.call(myUser); //Fix this
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 //Note(no tests)
